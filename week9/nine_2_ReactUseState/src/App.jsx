8dcc5283b6ffd305008e360e905768c715bab51b
Sejal-Jamwal <sejaljamwal1708@gmail.com>
2024-12-24 20:00:15 +0530

    Conditional Rendering, Unmounting, Cleanup

--- week9/nine_2_ReactUseState/src/App.jsx
+++ week9/nine_2_ReactUseState/src/App.jsx
@@ -1,10 +1,24 @@
+import { use } from 'react';
 import { useState, useEffect } from 'react'
 
 
 function App() {
 
+  //CONDITIONAL RENDERING
+
+  const [counterVisible, setCounterVisible] = useState(true);
+
+  //Logic for changing the counterVisible value after every 5 secs
+  useEffect(function(){
+     
+    setInterval(function(){
+       setCounterVisible(counterVisible => !counterVisible);
+    }, 5000);
+
+  }, []);
+
    return <div>
-      <Counter></Counter>
+       {counterVisible && <Counter></Counter>}
    </div>
    
 }
@@ -14,10 +28,17 @@ function Counter(){
    const [count, setCount] = useState(0);
     
    useEffect(function() {
-
-      setInterval(function(){
+      // Logic for when the component mounts, runs only when dependency array is empty
+      let clock = setInterval(function(){
         setCount(count => count + 1);
       }, 1000);
+
+      // UNMOUNTING AND CLEANUP
+
+      return function(){
+        //Logic for when the component unmounts, runs only when dependency array is empty
+        clearInterval(clock);
+      }
 
    }, []);
 
