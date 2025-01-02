81e32ce4f929d0e4879730dc1f3db920dafff760
Sejal-Jamwal <sejaljamwal1708@gmail.com>
2025-01-02 22:49:41 +0530

    Layouts in React

--- week10-Routing/src/App.jsx
+++ week10-Routing/src/App.jsx
@@ -1,22 +1,57 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
 
   return <div>
     <BrowserRouter>
-      <Link to="/">Allen</Link>
+      
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+
+        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element ={<ErrorPage/>}/>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
+}
+
+function Layout(){
+
+  return <div style={{height: "100vh", background:"green"}}>
+
+      <Header/>
+
+      <div style={{height: "90vh", background:"red"}}>
+        <Outlet/>
+      </div>
+
+      Footer
+
+  </div>
+     
+}
+
+function Header(){
+    
+  return  <div>
+    <Link to="/">Allen</Link>
       | 
       <Link to="/neet/online-coaching-class-11">Class 11</Link> 
       | 
       <Link to="/neet/online-coaching-class-12">Class 12</Link>
-      <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+
+  </div>
+}
+
+function ErrorPage(){
+  return <div>
+     No such page exists!
   </div>
 }
 
